6958871b073fd7d629e7a441b73601c3ea286cf1
Arthur <arthur_sbd@hotmail.com>
2019-05-08 14:46:59 -0300

    it 3

--- starter-code/src/components/FoodBox.jsx
+++ starter-code/src/components/FoodBox.jsx
@@ -1,48 +1,23 @@
-// import foods from './foods.json'
+import foods from './../foods.json'
 import React, { Component } from 'react';
+import FoodCard from './FoodCard.jsx';
 
 class FoodBox extends Component {
   constructor() {
     super();
     this.state = {
-
+      arrfoods: foods
     }
   }
 
   render() {
     return (
-      <div className="box">
-        <article className="media">
-          <div className="media-left">
-            <figure className="image is-64x64">
-              <img src="https://i.imgur.com/eTmWoAN.png" />
-            </figure>
-          </div>
-          <div className="media-content">
-            <div className="content">
-              <p>
-                <strong>Pizza</strong> <br />
-                <small>400 cal</small>
-              </p>
-            </div>
-          </div>
-          <div className="media-right">
-            <div className="field has-addons">
-              <div className="control">
-                <input
-                  className="input"
-                  type="number" 
-                  value="1"
-                />
-              </div>
-              <div className="control">
-                <button className="button is-info">
-                  +
-                </button>
-              </div>
-            </div>
-          </div>
-        </article>
+      <div className="foods-container">
+        {
+        this.state.arrfoods.map((OneFood, index) => {
+          return <FoodCard key={index} {...OneFood}/>
+        })
+      }
       </div>
     )  
   }
